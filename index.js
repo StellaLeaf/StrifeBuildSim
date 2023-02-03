6309bc0376a1139db9ec6333a90af0cef92a99bb
StellaLeaf <youliang846@gmail.com>
2023-02-03 21:22:28 +0900

    update for node.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-//Result[0Av,1AvHs,2Hi,3HiHs,4Cap,5Relo,6Rate,7Dps,8Sprd,9Ads,10Wt,11Dura,12CrAv,13CrHi,14C10m,15CMax]
-var Result = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-//Weapon[0Dmg,1HsB,2Cap,3Relo,4RlSt,5Rate,6Plet,7Sprd,8Ads,9Wt]
-var Weapon = [0,0,0,0,0,0,0,0,0,0];
-//Mod[0Dmg,1HsB,2Cap,3Relo,4Rate,5Sprd,6Wt]
-var Mod = [0,0,0,0,0,0,0];
-//Ench[0Av,1Hi,2OeAv,3OeHi]
-var Ench = [0,0,0,0];
-//AE[0Av,1Hi]
-var Ae = [0,0];
-//Addon[0Av,1Hi,2Cap,3Relo,4Wt]
-var Addon = [0,0,0,0,0]
-
 //計算
 function ResultCalc(){
   Result[0] = Math.round( (Weapon[0] + Mod[0] + Ench[0] + Ench[2] + Ae[0] + Addon[0] + Result[12]) * Math.pow( 10, 1 ) ) / Math.pow( 10, 1 ) ;
@@ -58,7 +45,7 @@ function WeaponMod(){
   var WeaponSelect2 = document.getElementById('WeaponForm').WeaponSelect.value;
   var ModSelect2 = document.getElementById('ModForm').ModSelect.value;
   switch (WeaponSelect2) {
-    case 'Serenity Auto Rifle':
+    case 'Serenity':
       Weapon[0] = SerenityCs[0];
       Weapon[1] = SerenityCs[1];
       Weapon[2] = SerenityCs[2];
@@ -72,7 +59,7 @@ function WeaponMod(){
       Result[13] = SerenityCs[10];
       Result[14] = SerenityCs[11];
       Result[15] = SerenityCs[12];
-      Weapon[9] = SerenityCs[13];
+      Weapon[9] = SerenityCsp[8];
       switch (ModSelect2){
         case 'Monarch':
           Mod[0] = SerenityCsp[0];
@@ -102,7 +89,7 @@ function WeaponMod(){
           break;
       };
       break;
-    case 'F44 Dominance':
+    case 'Dominance':
       Weapon[0] = DominanceCs[0];
       Weapon[1] = DominanceCs[1];
       Weapon[2] = DominanceCs[2];
@@ -116,7 +103,7 @@ function WeaponMod(){
       Result[13] = DominanceCs[10];
       Result[14] = DominanceCs[11];
       Result[15] = DominanceCs[12];
-      Weapon[9] = DominanceCs[13];
+      Weapon[9] = DominanceCsp[8];
       switch (ModSelect2){
         case 'Monarch':
           Mod[0] = DominanceCsp[0];
@@ -146,7 +133,7 @@ function WeaponMod(){
           break;
       };
       break;
-    case 'Warmonger':
+    case 'warmonger':
       Weapon[0] = WarmongerCs[0];
       Weapon[1] = WarmongerCs[1];
       Weapon[2] = WarmongerCs[2];
@@ -160,7 +147,7 @@ function WeaponMod(){
       Result[13] = WarmongerCs[10];
       Result[14] = WarmongerCs[11];
       Result[15] = WarmongerCs[12];
-      Weapon[9] = WarmongerCs[13];
+      Weapon[9] = WarmongerCsp[8];
       switch (ModSelect2){
         case 'Monarch':
           Mod[0] = WarmongerCsp[0];
@@ -190,7 +177,7 @@ function WeaponMod(){
           break;
       };
       break;
-    case 'WOUND Auto Rifle':
+    case 'wound':
       Weapon[0] = WoundCs[0];
       Weapon[1] = WoundCs[1];
       Weapon[2] = WoundCs[2];
@@ -234,7 +221,7 @@ function WeaponMod(){
           break;
       };
       break;
-    case 'AVG Auto Rifle':
+    case 'avg':
       Weapon[0] = AvgCs[0];
       Weapon[1] = AvgCs[1];
       Weapon[2] = AvgCs[2];
@@ -304,23 +291,185 @@ function WeaponMod(){
       break;
   };
 };
+const WeaponTypes=[
+  "AssaultRifle",
+  "SMG",
+  "Shotgun",
+  "LMG",
+  "SniperRifle",
+  "Carbine",
+  "Special",
+  "Staves",
+  "Secondary",
+  "Staves",
+  "その他"
+]
+const WeaponList = [
+  {category:'AssaultRifle', name:'A-12 Plastic Rifle', value:'weird'},
+  {category:'AssaultRifle', name:'AK Rifle', value:'cak'},
+  {category:'AssaultRifle', name:'AVG Auto Rifle', value:'avg'},
+  {category:'AssaultRifle', name:'Cilzret Fullauto', value:'Cilzret'},
+  {category:'AssaultRifle', name:'Divine Long Bow', value:'divine'},
+  {category:'AssaultRifle', name:'F44 Dominance', value:'Dominance'},
+  {category:'AssaultRifle', name:'FAZZI Automana', value:'fazzi'},
+  {category:'AssaultRifle', name:'GEO Mana Rifle', value:'geo'},
+  {category:'AssaultRifle', name:'Gerard Auto Rifle', value:'gerard'},
+  {category:'AssaultRifle', name:'Golden AK[フルオート]', value:'goldak'},
+  {category:'AssaultRifle', name:'Golden AK[セミオート]', value:'goldak2'},
+  {category:'AssaultRifle', name:'GROUZA', value:'groza'},
+  {category:'AssaultRifle', name:'Hamlock AR', value:'hemlock'},
+  {category:'AssaultRifle', name:'Harbinger Auto Rifle', value:'harbinger'},
+  {category:'AssaultRifle', name:'Hopekeeper', value:'hopekeeper'},
+  {category:'AssaultRifle', name:'JAME Manaburst Rifle', value:'jame'},
+  {category:'AssaultRifle', name:'Lexxi Burst Rifle', value:'lexxi'},
+  {category:'AssaultRifle', name:'Luna Crossbow', value:'lunabow'},
+  {category:'AssaultRifle', name:'M2002', value:'M2002'},
+  {category:'AssaultRifle', name:'MDR', value:'MDR'},
+  {category:'AssaultRifle', name:'MR-4 Smokey', value:'smokey'},
+  {category:'AssaultRifle', name:'MR-4 Smokey EXT', value:'cu_smokey'},
+  {category:'AssaultRifle', name:'MR-12 Automana', value:'automana'},
+  {category:'AssaultRifle', name:'MR-52 Cyclone', value:'cyclone'},
+  {category:'AssaultRifle', name:'MR-55 Stormfury', value:'stormfury'},
+  {category:'AssaultRifle', name:'MR-89 Doubletail', value:'Doubletail'},
+  {category:'AssaultRifle', name:'MR-94 Gargoyle', value:'gargoyle'},
+  {category:'AssaultRifle', name:'MRC-1 Silvermist', value:'Silvermist'},
+  {category:'AssaultRifle', name:'NURF Water Rifle', value:'waterar'},
+  {category:'AssaultRifle', name:'Ouroboros AR', value:'ouro'},
+  {category:'AssaultRifle', name:'Serenity Auto Rifle', value:'Serenity'},
+  {category:'AssaultRifle', name:'Starfaller', value:'starfaller'},
+  {category:'AssaultRifle', name:'The Shooting Star', value:'shootingstar'},
+  {category:'AssaultRifle', name:'Twinvision Rifle', value:'twinvision'},
+  {category:'AssaultRifle', name:'Type 89', value:'ctype'},
+  {category:'AssaultRifle', name:'Tyrant Automana', value:'Tyrant'},
+  {category:'AssaultRifle', name:'UDON-SKP Automana', value:'udon'},
+  {category:'AssaultRifle', name:'VK-74 FLETLINE', value:'fletline'},
+  {category:'AssaultRifle', name:'Warewolf AR', value:'warewolf'},
+  {category:'AssaultRifle', name:'Warmonger', value:'warmonger'},
+  {category:'AssaultRifle', name:'WOUND Auto Rifle', value:'wound'},
+  {category:'AssaultRifle', name:'XR-4 Auto Rifle', value:'xr4'},
+  {category:'SMG', name:'Anarchy', value:'anarchy'},
+  {category:'SMG', name:'Brawler Burst PDW', value:'brawler'},
+  {category:'SMG', name:'Brigit, the bringer of flame', value:'Brigit'},
+  {category:'SMG', name:'Demonflare', value:'demonflare'},
+  {category:'SMG', name:'Doorknocker', value:'door'},
+  {category:'SMG', name:'Drumming Victory', value:'cu_victory'},
+  {category:'SMG', name:'Envy', value:'Envy'},
+  {category:'SMG', name:'FW-5 Azuris PDW', value:'azuris'},
+  {category:'SMG', name:'Gabriel PDW', value:'Gabriel'},
+  {category:'SMG', name:'GG36 SMG', value:'gg36'},
+  {category:'SMG', name:'H.E.A.T 5', value:'heat5'},
+  {category:'SMG', name:'Hystis 535', value:'Hystis'},
+  {category:'SMG', name:'K92 Tiamat', value:'tiamat'},
+  {category:'SMG', name:'K95 Basilisk', value:'Basilisk'},
+  {category:'SMG', name:'Lynn SMG', value:'lynn'},
+  {category:'SMG', name:'Nox SMG', value:'nox'},
+  {category:'SMG', name:'P9000 Mana PDW', value:'P9000'},
+  {category:'SMG', name:"PP90N1 Dummy's", value:'pp90n1'},
+  {category:'SMG', name:"Rogue's Shooter", value:'rogues'},
+  {category:'SMG', name:'Silent Treatment', value:'Silent'},
+  {category:'SMG', name:'The Bumblebee', value:'bumblebee'},
+  {category:'SMG', name:'Thousand Cut', value:'thousand'},
+  {category:'SMG', name:'Type6 Hicycle PDW', value:'type6'},
+  {category:'SMG', name:'Victory', value:'victory'},
+  {category:'SMG', name:'VMT-2 Mice SMG', value:'vmt'},
+  {category:'SMG', name:'Windmaker', value:'windmaker'},
+  {category:'SMG', name:'X-1 Ammit', value:'Ammit'},
+  {category:'SMG', name:'XS-5 Nether Beatle', value:'netherbeatle'},
+  {category:'SMG', name:'XS-21 Pandora PDW', value:'pandora'},
+  {category:'Shotgun', name:'AA122 Mana Shotgun', value:'aa12'},
+  {category:'Shotgun', name:"Bazl's Blunderbuss", value:'bazl'},
+  {category:'Shotgun', name:'Black Thunder', value:'blackThunder'},
+  {category:'Shotgun', name:'Blondie[散弾]', value:'blond'},
+  {category:'Shotgun', name:'Blondie[フラグ]', value:'blond2'},
+  {category:'Shotgun', name:'Crescent of Asha', value:'Asha'},
+  {category:'Shotgun', name:'Crimson', value:'Crimson'},
+  {category:'Shotgun', name:'E420 Hydra', value:'Hydra'},
+  {category:'Shotgun', name:'F-85 The Raven', value:'raven'},
+  {category:'Shotgun', name:'Gauss Rifle', value:'cgauss'},
+  {category:'Shotgun', name:"Ghahr's Shotgun", value:'Ghahr'},
+  {category:'Shotgun', name:"Howard's Double Barrel", value:'howard'},
+  {category:'Shotgun', name:'K-RAR Mana Splasher', value:'krar'},
+  {category:'Shotgun', name:'LF-14 Auto Shotgun', value:'lf14'},
+  {category:'Shotgun', name:'Lios Double Barrel', value:'lios'},
+  {category:'Shotgun', name:"Lusha's Harp", value:'harp'},
+  {category:'Shotgun', name:'M890 Beanshot', value:'m890'},
+  {category:'Shotgun', name:'Ogrehunt', value:'ogrehunt'},
+  {category:'Shotgun', name:'Ogrehunt Slug', value:'sogrehunt'},
+  {category:'Shotgun', name:'OgreKiller', value:'cu_ogrehunt'},
+  {category:'Shotgun', name:'Pianis Mana Splasher', value:'Pianis'},
+  {category:'Shotgun', name:'Pianis Mana Splasher Slug', value:'sPianis'},
+  {category:'Shotgun', name:'Sabertooth Shotgun', value:'sabertooth'},
+  {category:'Shotgun', name:'SG80 Evicserate', value:'evis'},
+  {category:'Shotgun', name:'Spacemaker', value:'spacemaker'},
+  {category:'Shotgun', name:'STUD Shotgun', value:'stud'},
+  {category:'Shotgun', name:'The 1980', value:'1990'},
+  {category:'Shotgun', name:'The Extra', value:'extra'},
+  {category:'Shotgun', name:'The Thunder', value:'Thunder'},
+  {category:'Shotgun', name:'Troublemaker Shotgun', value:'trouble'},
+  {category:'LMG', name:'Brutus MG', value:'Brutus'},
+  {category:'LMG', name:'GILT Hypercycle LMG[高レート]', value:'gilt'},
+  {category:'LMG', name:'GILT Hypercycle LMG[高精度]', value:'gilt2'},
+  {category:'LMG', name:'Gorgon 114 LMG', value:'Gorgon'},
+  {category:'LMG', name:'Life Barrage Wand', value:'lifebarrage'},
+  {category:'LMG', name:'M666 Spatfire', value:'spit'},
+  {category:'LMG', name:'M757 Cleaner LMG', value:'M757'},
+  {category:'LMG', name:'M9190 Beehive', value:'bee'},
+  {category:'LMG', name:'MG-80 Heavy Machinegun', value:'MG80'},
+  {category:'LMG', name:'MK4 Machine Gun', value:'mk4'},
+  {category:'LMG', name:'MK7 Baretooth', value:'baretooth'},
+  {category:'LMG', name:'MK18 Leaf Cleaner', value:'leafcleaner'},
+  {category:'LMG', name:'PBP-14', value:'PBP14'},
+  {category:'LMG', name:'Silverstein MG', value:'silverstein'},
+  {category:'LMG', name:'TT4 Nefaris LMG', value:'nefaris'},
+  {category:'LMG', name:'TT4 Nefaris MMG', value:'cu_nefaris'},
+  {category:'LMG', name:'	TT66 Doom LMG', value:'TT666'},
+  {category:'LMG', name:'XL9 Mozer', value:'mozer'},
+];
+const categorySelect2 = document.getElementById('TypeSelect');
+const subCategorySelect2 = document.getElementById('WeaponSelect');
+WeaponTypes.forEach(category => {
+  const option = document.createElement('option');
+  option.textContent = category;
 
+  categorySelect2.appendChild(option);    
+});
+categorySelect2.addEventListener('input', () => {
 
-//武器データ
-//Cs[0Dmg,1HsBonus,2Capacity,3Reload,4ReloadStyle,5Rate,6pellets,7Spread,8ADS,9CritPer,10CritDmg,11Change10m,12MaxChange,13Weight]
-const SerenityCs = [13,4,32,50,0,15,1,0.6,0.15,0,0,-0.89,-6,0];
-const DominanceCs = [9,1,25,60,0,20,1,0.8,0.1,0,0,-0.98,-3,0];
-const WarmongerCs = [8,7,28,50,0,10,1,0.25,0,0.25,6,0,0,0];
-const WoundCs = [11,4,20,50,0,8,1,0.55,0.2,0.3,8,0,0,0];
-const AvgCs = [16,4,25,50,1,12,1,0.6,0.1,0.3,3,-0.95,-6,0];
-//Csp[0MonaDmg,1MonaRate,2MonaSpread,3EmpHs,4EmpCapacity,5EmpWeight,6WlCapacity,7WlReload]
-const SerenityCsp = [2,2,-0.3,3,10,0.015,20,-30];
-const DominanceCsp = [2,0,-0.3,3,10,0.015,20,-30];
-const WarmongerCsp = [2,2,0,6,10,0.015,20,-30];
-const WoundCsp = [3,2,-0.3,4,10,0.015,20,-30];
-const AvgCsp = [2,2,-0.3,3,10,0.015,20,-30];
+  // 小分類のプルダウンをリセット
+  const options = document.querySelectorAll('#WeaponSelect > option');
+  options.forEach(option => {
+    option.remove();
+  });
 
+  // 小分類のプルダウンに「選択してください」を加える
+  const firstSelect = document.createElement('option');
+  firstSelect.textContent = '武器を選択してください';
+  firstSelect.setAttribute('value',null);
+  subCategorySelect2.appendChild(firstSelect);
+
+  // 小分類を選択（クリック）できるようにする
+  subCategorySelect2.disabled = false;
+
+  // 大分類が選択されていない（「選択してください」になっている）とき、小分類を選択（クリック）できないようにする
+  if (categorySelect2.value == 'blank') {
+    subCategorySelect2.disabled = true;
+    return;
+  }
+  // 大分類で選択されたカテゴリーと同じ小分類のみを、プルダウンの選択肢に設定する
+  WeaponList.forEach(subCategory => {
+    if (categorySelect2.value == subCategory.category) {
+      const option = document.createElement('option');
+      option.textContent = subCategory.name;
+      option.setAttribute('value',subCategory.value)
+      subCategorySelect2.appendChild(option);
+    };
+  });
+});
 //トリガー
+document.getElementById('TypeForm').onchange = function() {
+  WeaponMod();
+  ResultCalc();
+};
 document.getElementById('WeaponForm').onchange = function() {
   WeaponMod();
   ResultCalc();
@@ -329,15 +478,6 @@ document.getElementById('ModForm').onchange = function() {
   WeaponMod();
   ResultCalc();
 };
-
-//Enchデータ
-const SfAverageDmg =[2.01,2.16,2,31]
-const SfHighestDmg = 3
-const DpAverageDmg =[1.4,1.6,1.8]
-const DpHighestDmg = 4;
-const SlAverageDmg = [0.8,1.0,1.2]
-const SlHighestDmg = 2;
-
 //エンチャダメージ処理
 document.getElementById('EnchantForm').onchange = function() {
     switch (document.getElementById('EnchantForm').EnchantSelect.value) {
@@ -408,7 +548,6 @@ document.getElementById('EnchantForm').onchange = function() {
     };
     ResultCalc();
 };
-
 //OEダメージ処理
 document.getElementById('OeForm').onchange = function() {
   switch (document.getElementById('OeForm').OeSelect.value) {
@@ -479,15 +618,6 @@ document.getElementById('OeForm').onchange = function() {
   };
   ResultCalc();
 };
-
-//AEデータ
-const BcDmg = 7;
-const EoDmg = 6;
-const McDmg = 1;
-const CfDmg = 4;
-const SdAverageDmg = 1;
-const SdHighestDmg = 20;
-
 //AEダメージ処理
 document.getElementById('AncientEnchantForm').onchange = function() {
     switch (document.getElementById('AncientEnchantForm').AncientEnchantSelect.value) {
@@ -517,15 +647,6 @@ document.getElementById('AncientEnchantForm').onchange = function() {
     };
     ResultCalc();
 };
-
-//Addonデータ
-const MpDmg =[0.25,0.5,1.0,1.5]
-const HbAverageDmg =[0.4,0.8,1.2,1.6]
-const HbHighestDmg =[2,4,6,8]
-const EmCapacity =[2,4,6,10]
-const QpReload =[-4,-8,-12,-16]
-const LkWeight =[0.004,0.005,0.010,0.015]
-
 //Addonダメージ処理
 document.getElementById('AddonForm').onchange = function() {
   switch (document.getElementById('AddonForm').AddonSelect.value) {
